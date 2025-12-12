09182043989313803b5b338a4633df4558aec6be
shana-dude <rakshana.m2024aids@sece.ac.in>
2025-12-12 11:50:31 +0530

    url in protect

--- conflict/db.js
+++ conflict/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/mydb');
+    await mongoose.connect(process.env.DB_url);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
